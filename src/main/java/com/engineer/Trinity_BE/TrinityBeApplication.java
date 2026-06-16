1b1f72caa3aca44db172b5dfeaa874fc7e37ea58
junsik <133089807+JunsikHong@users.noreply.github.com>
2026-06-16 17:59:39 +0900

    core : 프로젝트 시작

---
+++ src/main/java/com/engineer/Trinity_BE/TrinityBeApplication.java
@@ -0,0 +1,13 @@
+package com.engineer.Trinity_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrinityBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrinityBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/engineer/Trinity_BE/TrinityBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engineer.Trinity_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrinityBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
