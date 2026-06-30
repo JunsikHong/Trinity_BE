8da63d3c8cb6a454240d14b9466852d04250c6db
junsik <133089807+JunsikHong@users.noreply.github.com>
2026-06-30 18:09:15 +0900

    feat : 입력값 동적 생성

--- src/main/java/com/engineer/Trinity_BE/domain/maintenance/entity/MaintenanceReport.java
+++ src/main/java/com/engineer/Trinity_BE/domain/maintenance/entity/MaintenanceReport.java
@@ -33,42 +33,6 @@ public class MaintenanceReport {
     @JoinColumn(name = "airplane_id",  nullable = false)
     private Airplane airplane;
 
-    // 정비 매뉴얼 분류 번호
-    @Column(name = "chapter")
-    private Double chapter;
-
-    // 기수에서 꼬리방향 (x축) 위치
-    @Column(name = "station")
-    private Double station;
-
-    // 아래에서 위 방향 (z축) 위치
-    @Column(name = "water_line")
-    private Double waterLine;
-
-    // 항공기 중심선 기준 좌우 (y축) 위치
-    @Column(name = "buttock_line")
-    private Double buttockLine;
-
-    // 동체 길이 방향 보강제 번호
-    @Column(name = "stringer")
-    private Double stringer;
-
-    // 동체 단면 방향 구조물 번호
-    @Column(name = "frame")
-    private Double frame;
-
-    // 날개 내부 단면 구조물 번호
-    @Column(name = "rib")
-    private Double rib;
-
-    // 날개 방향 위치
-    @Column(name = "wing_station")
-    private Double wingStation;
-
-    // 동체 방향 위치
-    @Column(name = "body_station")
-    private Double bodyStation;
-
     @Column(name = "description")
     private String description;
 
@@ -81,26 +45,8 @@ public class MaintenanceReport {
     private LocalDateTime updatedAt;
 
     public void update(Airplane airplane,
-                       Double chapter,
-                       Double station,
-                       Double waterLine,
-                       Double buttockLine,
-                       Double stringer,
-                       Double frame,
-                       Double rib,
-                       Double wingStation,
-                       Double bodyStation,
                        String description) {
         this.airplane = airplane;
-        this.chapter = chapter;
-        this.station = station;
-        this.waterLine = waterLine;
-        this.buttockLine = buttockLine;
-        this.stringer = stringer;
-        this.frame = frame;
-        this.rib = rib;
-        this.wingStation = wingStation;
-        this.bodyStation = bodyStation;
         this.description = description;
     }
 
